e231b70edbb07fdc4ce058bc84b254d0f383e0fa
Ambition <528289176@qq.com>
2020-10-10 23:29:33 +0800

    improve article add-article and all article page

--- src/views/article/add-article.vue
+++ src/views/article/add-article.vue
@@ -1,11 +1,71 @@
 <template>
   <div class="container">
-    <p>sada</p>
+    <el-card class="head-card">
+      <el-page-header @back="goBack" content="编辑文章">
+      </el-page-header>
+      <el-input
+      class="title-input"
+      placeholder="请输入文章标题">
+      </el-input>
+      <el-dropdown @command="select">
+        <el-button type="primary">
+          切换编辑器<i class="el-icon-arrow-down el-icon--right"></i>
+        </el-button>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item command="rte">富文本编辑器</el-dropdown-item>
+          <el-dropdown-item command="markdown">MarkDown编辑器</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </el-card>
+    <mavon-editor :v-if="markdown"/>
+    <tinymce :v-if="rte"></tinymce>
   </div>
 </template>
+
+<script>
+import MavonEditor from '@/components/MavonEditor'
+import Tinymce from '@/components/Tinymce'
+
+export default {
+  components: {
+    MavonEditor,
+    Tinymce
+  },
+  data() {
+    return {
+      rte: false,
+      markdown: true
+    }
+  },
+  methods: {
+    goBack() {  
+      this.$router.go(-1)
+    },
+    select(data) {
+      if(data == 'markdown'){
+        this.markdown = true
+        this.rte = false
+      } else {
+        this.markdown = false
+        this.rte = true
+      }
+      console.log(data + this.markdown + " " + this.rte)
+    }
+  }
+}
+</script>
 
 <style scoped>
   .container {
     margin: 10px;
+  }
+
+  .title-input {
+    margin-top: 20px;
+    margin-bottom: 30px;
+  }
+
+  .head-card {
+    margin-bottom: 20px;
   }
 </style>
--- src/views/article/all-article.vue
+++ src/views/article/all-article.vue
@@ -5,33 +5,30 @@
       <el-tab-pane label="已发布"></el-tab-pane>
       <el-tab-pane label="草稿"></el-tab-pane>
       <el-tab-pane label="回收站"></el-tab-pane>
-      <el-button type="primary">批量操作</el-button>
-      <el-button type="primary">批量操作</el-button>
-      <el-button type="primary">批量操作</el-button>
-      <el-button type="primary">批量操作</el-button>
-      <el-button type="primary">批量操作</el-button>
-      <el-table
-        :data="tableData"
-        style="width: 100%">
-        <el-table-column
-          prop="title"
-          label="标题">
-        </el-table-column>
-        <el-table-column
-          prop="meta"
-          label="分类目录">
-        </el-table-column>
-        <el-table-column
-          prop="message"
-          label="留言">
-        </el-table-column>
-        <el-table-column
-          prop="postDate"
-          label="发布日期">
-        </el-table-column>
-        <el-table-column
-          prop="postModified"
-          label="最后更新">
+      <div >
+        <el-button type="primary">批量操作</el-button>
+        <el-button type="primary">批量操作</el-button>
+        <el-button type="primary">批量操作</el-button>
+        <div class="search">
+          <el-input
+            :input="keyword"
+            clearable
+            placeholder="搜索文章">
+            <el-button slot="append" type="success">搜索</el-button>  
+          </el-input>
+        </div>
+      </div>
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="title" label="标题"> </el-table-column>
+        <el-table-column prop="meta" label="分类目录"> </el-table-column>
+        <el-table-column prop="message" label="留言"> </el-table-column>
+        <el-table-column prop="postDate" label="发布日期"> </el-table-column>
+        <el-table-column prop="postModified" label="最后更新"> </el-table-column>
+        <el-table-column label="操作" >
+          <template slot-scope="scope">
+            <el-button size="small" type="primary" style="margin-left: 10px" @click="handleEdit(scope.$index, scope.row)">备注</el-button>
+            <el-button size="small" type="danger" style="margin-top: 5px" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
         </el-table-column>
       </el-table>
     </el-tabs>
@@ -42,18 +39,31 @@
 export default {
   data() {
     return {
-      tableData:[]
-    }
-  }
-}
+      tableData: [],
+      keyword: "",
+    };
+  },
+  methods: {
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      console.log(index, row);
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .container {
-    margin: 15px;
-  }
-  
-  .tabs {
-    margin-bottom: 20px;
-  }
+.container {
+  margin: 15px;
+}
+
+.tabs {
+  margin-bottom: 20px;
+}
+
+.search {
+  float: right;
+}
 </style>
