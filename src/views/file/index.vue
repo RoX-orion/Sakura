df5596e5fbfe49b68a63253dc6e6d5c242299629
Ambition <528289176@qq.com>
2020-10-18 22:52:01 +0800

    Add dialog to look detailed data of the file

--- src/views/file/index.vue
+++ src/views/file/index.vue
@@ -18,15 +18,38 @@
           <el-button type="success" size="medium">下载文件<svg-icon icon-class="download"></svg-icon></el-button>
         </div>
       </el-col>
-      <el-col :lg="4" v-for="item in fileList" 
-          :key="item.url">
-        <el-image
-          class="list"
-          
-          :src="item.url">
-        </el-image>
+      <el-col :lg="4" :md="6" :sm="6" :xs="12"
+        class="repo"
+        v-for="item in fileList" 
+        :key="item.url">
+        <el-button class="img" @click="detailed(item)">
+          <el-image
+            class="list"
+            :src="item.url">
+          </el-image>
+        </el-button>
+        <div class="filename">{{item.name}}</div>
       </el-col>
     </el-row>
+    <el-dialog :visible.sync="dialogVisible" width="90%" :v-bind="detailedData">
+      <el-row :gutter="20">
+        <el-col :lg="15">
+          <el-image
+          :src="detailedData.url">
+        </el-image>
+        </el-col>
+        <el-col :lg="9">
+          <div>
+            <div class="attribute">文件名: {{detailedData.name}}</div>
+            <div class="attribute">文件类型: {{detailedData.mimeType}}</div>
+            <div class="attribute">上传时间: {{detailedData.uploadDate}}</div>
+            <div class="attribute">文件大小: {{detailedData.fileSize}}</div>
+            <div class="attribute">存储位置: {{storagePos}}</div>
+            <el-input :value="detailedData.url"></el-input>
+          </div>
+        </el-col>
+      </el-row>
+    </el-dialog>
   </div>
 </template>
 
@@ -40,6 +63,9 @@ export default {
   },
   data() {
     return {
+      storagePos: '',
+      detailedData: {},
+      dialogVisible: false,
       lazy: true,
       fileList: [],
       drawer: false,
@@ -58,6 +84,15 @@ export default {
           this.fileList = data.list
         }
       })
+    },
+    detailed(data) {
+      this.dialogVisible = true
+      this.detailedData = data
+      if(data.storagePos == 'local'){
+        this.storagePos = '本地服务器'
+      } else {
+        this.storagePos = '对象存储'
+      }
     }
   }
 }
@@ -69,9 +104,32 @@ export default {
   }
 
   .list {
-    width: 200px;
-    height: 200px;
+    width: 150px;
+    height: 150px;
     overflow: hidden;
+  }
+
+  .img {
+    padding: 10px;
+  }
+
+  .list:hover {
+    box-shadow: rgba(0, 0, 256, 1);
+  }
+
+  .filename {
+    margin-bottom: 20px;
+    /* width: 80%; */
+    text-align: center;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1;
+  }
+
+  .attribute {
+    margin-bottom: 10px;
   }
 </style>
 
